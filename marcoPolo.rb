a7592d7efad780b21b7866c36539d251e9fed3a6
RobotMermaid <ondinerangel@gmail.com>
2017-03-27 10:50:26 -0700

    cleaned up

--- marcoPolo.rb
+++ marcoPolo.rb
@@ -26,11 +26,9 @@ end
 browser = Capybara.current_session
 browser.visit 'http://10.0.0.1'
 browser.fill_in(@userNameField, with: @userName)
-# browser.fill_in('loginUsername', with: 'admin')
 browser.fill_in(@userPasswordField, with: @userPassword)
 browser.click_button(@login)
 puts browser.current_url
-# "http://10.0.0.1/at_a_glance.asp"
 deviceTable = browser.find_by_id("internet-usage")
 devicesAllDivs = browser.within(deviceTable) {browser.all 'div.form-row'}
 deviceList = {}
@@ -46,7 +44,6 @@ puts "shiny! let's go be bad guys"
 
 file = File.read('devicesMinsAgo.json')
 devices_old = JSON.parse(file)
-difference = []
 email_string = ""
 
 if deviceList.keys != devices_old.keys
@@ -66,5 +63,5 @@ puts email_string
 if (email_string.length > 0)
   `curl -X POST -H "Content-Type: application/json" -d \
   '{"value1":"#{email_string}"}' \
-  https://maker.ifttt.com/trigger/network_change/with/key/pzoBEghOg1aYYaHGEWam9HfN6wjdk3u02qIRN2kVAVN`
+  https://maker.ifttt.com/trigger/@eventName/with/key/@apiKey`
 end
